dacf214d0bb840758e634b84710672b215560a0d
Leynos <leynos@troubledskies.net>
2014-06-15 23:46:57 +0100

    License header added

--- js/srpg.js
+++ js/srpg.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2014, David McIntosh <dmcintosh@df12.net>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 
 var $ = require("jquery");
 
@@ -31,6 +46,7 @@ var Tiler = function(context, src, tileW, tileH) {
     };
 };
 
+// The "Game" object
 var Gambit = function(canvas) {
     var context = canvas.getContext("2d");
     var viewportW = canvas.width;
