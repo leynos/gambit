df1ad6fb7f143a213ba927a2300ddb8b5538ce5c
Leynos <leynos@troubledskies.net>
2014-07-01 00:42:51 +0100

    Simple layered tiling.

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-js/main.js:
+js/main.js: js/srpg.js
 	browserify js/srpg.js -o js/main.js
+
+.PHONY: clean 
+clean:
+	rm js/main.js
--- js/srpg.js
+++ js/srpg.js
@@ -16,12 +16,47 @@
 
 var $ = require("jquery");
 
+var tufts = {};
+tufts[parseInt('100000000', 2)] = {x: 6, y: 11};
+tufts[parseInt('010000000', 2)] = {x: 5, y: 11};
+tufts[parseInt('001000000', 2)] = {x: 4, y: 11};
+tufts[parseInt('000100000', 2)] = {x: 6, y: 10};
+tufts[parseInt('000010000', 2)] = {x: 5, y: 10};
+tufts[parseInt('000001000', 2)] = {x: 4, y: 10};
+tufts[parseInt('000000100', 2)] = {x: 6, y: 9 };
+tufts[parseInt('000000010', 2)] = {x: 5, y: 9 };
+tufts[parseInt('000000001', 2)] = {x: 4, y: 9 };
+tufts[parseInt('010001000', 2)] = {x: 6, y: 8 };
+tufts[parseInt('000001010', 2)] = {x: 6, y: 7 };
+tufts[parseInt('010100000', 2)] = {x: 7, y: 8 };
+tufts[parseInt('000100010', 2)] = {x: 7, y: 7 };
+
+var map = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+
 // Encapsulate tile drawing methods
 var Tiler = function(context, src, tileW, tileH) {
     this.image = new Image();
     this.image.src = src;
     this.tileW = tileW;
     this.tileH = tileH;
+    this.map = null;
 
     // Draw a single tile (referncing X, Y pos on the spritesheet) at the given
     // destination position
@@ -34,13 +69,74 @@ var Tiler = function(context, src, tileW, tileH) {
             destW, destH);
     };
 
+    // Returns a list of locations adjacent to a given location affecting which
+    // tile should be drawn
+    this.adjacency = function(x, y) {
+        var adj = [];
+        if (x < map.length-1 && this.map[x+1][y+0]) {
+            adj.push(8);
+        }
+        if (y < map.length-1 && this.map[x+0][y+1]) {
+            adj.push(2);
+        }
+        if (x > 0 && this.map[x-1][y+0]) {
+            adj.push(32);
+        }
+        if (y > 0 && this.map[x+0][y-1]) {
+            adj.push(128);
+        }
+        if (adj.length > 0) {
+            return adj;
+        }
+        if (x < map.length-1 && y < map.length-1 && this.map[x+1][y+1]) {
+            adj.push(1);
+        }
+        if (y < map.length-1 && x > 0 && this.map[x-1][y+1]) {
+            adj.push(4);
+        }
+        if (x > 0 && y > 0 && this.map[x-1][y-1]) {
+            adj.push(256);
+        }
+        if (x < map.length-1 && y > 0 && this.map[x+1][y-1]) {
+            adj.push(64);
+        }
+        return adj;
+    }
+
+    // Returns a list of tile sprites that should be applied to a given map
+    // location
+    this.getTiles = function(x, y) {
+        var grass = {x: 14, y: 4};
+        if (null === this.map) {
+            return [grass];
+        }
+
+        if (this.map[x][y]) {
+            return [{x: 5, y: 10}];
+        }
+
+        var adj = this.adjacency(x, y);
+        var key = 0;
+        var tiles = [grass];
+        if (adj.length > 1) {
+            key = adj[0] | adj[1];
+            tiles.push(tufts[key]);
+        } else if (adj.length == 1 ) {
+            key = adj[0];
+            tiles.push(tufts[key]);
+        }
+        return tiles;
+    }
+
     // Draw the map in its entirity.  Later, we'll add clipping options, etc.
     this.drawMap = function(offsetX, offsetY) {
         for (var i=0; i<16; i++) {
             for (var j=0; j<16; j++) {
-                // For now, we're just drawing boring grass
-                this.draw(14, 4, 
-                    i*this.tileW + offsetX, j*this.tileH + offsetY, 32, 32);
+                var tiles = this.getTiles(i, j);
+                tiles.forEach(function(tile) {
+                    this.draw(tile.x, tile.y, 
+                        i*this.tileW + offsetX, j*this.tileH + offsetY, 32, 32);
+                }, this);
             }
         }
     };
@@ -58,6 +154,7 @@ var Gambit = function(canvas) {
     };
 
     this.run = function() {
+        tiler.map = map;
         tiler.drawMap(0, 0);
 
         // Add a listener for dragging
